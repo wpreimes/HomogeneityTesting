correct k,d calculation, add extended ts option, add 10D data usage

adjustment.py
@@ -10,11 +10,11 @@ import scipy.stats as stats
 import pandas as pd
 
 def compare_B(B, adjust_param, tolerance = 0.05):
-    p1_B1_after = B[0][0] #add
-    p1_B2_after = B[0][1] #mult
+    p1_B1_after = B[0,0] #add
+    p1_B2_after = B[1,0] #mult
 
-    p2_B1_after = B[1][0] #add
-    p2_B2_after = B[1][1] #mult
+    p2_B1_after = B[0,1] #add
+    p2_B2_after = B[1,1] #mult
 
     if adjust_param == 'both':
         if not np.isclose(p1_B1_after, p2_B1_after, atol=tolerance) or \
@@ -33,8 +33,8 @@ def compare_B(B, adjust_param, tolerance = 0.05):
         else:
             return True
 
-def adjustment(data_daily, B, breaktime, adjust_param = 'both', adjust_part='first', return_part='all',
-               check_adjustment=False, nbr_rec=0):
+def adjustment(data_daily, B, breaktime,
+               adjust_param = 'both', adjust_part='first', return_part=None):
     '''
     :param data: pd.DataFrame
         from Homogeneity Testing, must not contain nan
@@ -51,75 +51,51 @@ def adjustment(data_daily, B, breaktime, adjust_param = 'both', adjust_part='fir
         'last' to return only data after breaktime
     :return: pd.DataFrame, dict
     '''
+    if not return_part:
+        return_part = adjust_part
 
     dataframe = data_daily.copy()
 
+    B = np.matrix([[B['b1'][0], B['b2'][0]],
+                   [B['b1'][1], B['b2'][1]]])
+
+    part1 = dataframe[['testdata']][:breaktime]
+    part2 = dataframe[['testdata']][breaktime + pd.DateOffset(1):]
 
     if adjust_part == 'last':
         # Perform the linear adjustment for testdata after the breaktime
-        cc = B[0][1] / B[1][1]
-        dd = B[0][0] - cc * B[1][0]
-        adjusted_part1 = dataframe[['testdata']][:breaktime]
 
-        if adjust_param == 'both':
-            adjusted_part2 = cc * dataframe[['testdata']][breaktime:] + dd
-        elif adjust_param == 'add':
-            adjusted_part2 = dataframe[['testdata']][breaktime:] + dd
-        elif adjust_param == 'mult':
-            adjusted_part2 = cc * dataframe[['testdata']][breaktime:]
-        adjusted_part2 = adjusted_part2[breaktime + pd.DateOffset(1):]
+        cc = B[0, 0] / B[0, 1]  # k1/k2
+        dd = B[1, 0] - cc * B[1, 1] # d1 - cc*d2
 
+
+        if adjust_param == 'd':
+            adjusted_part2 = (cc * part2 + dd) / cc
+        else:
+            adjusted_part2 = cc * part2 + dd
+
+        dataframe['adjusted'] = pd.concat([part1, adjusted_part2])
 
     elif adjust_part == 'first':
         # Perform the linear adjustment for testdata before the breaktime
-        cc = B[1][1] / B[0][1]
-        dd = B[1][0] - cc * B[0][0]
-        adjusted_part2 = dataframe[['testdata']][breaktime + pd.DateOffset(1):]
-        if adjust_param == 'both':
-            adjusted_part1 = cc * dataframe[['testdata']][:breaktime] + dd
-        elif adjust_param == 'add':
-            adjusted_part1 = dataframe[['testdata']][:breaktime] + dd
-        elif adjust_param == 'mult':
-            adjusted_part1 = cc * dataframe[['testdata']][:breaktime]
-        adjusted_part1 = adjusted_part1
 
+        # B =   | k1  k2|
+        #       | d1  d2 |
+
+        cc = B[0, 1] / B[0, 0]  # k2/k1
+        dd = B[1, 1] - cc * B[1, 0] # d2 - cc*d1
+
+        if adjust_param == 'd':
+            meandiff = part2.mean() - part1.mean()
+            adjusted_part1 = part1 + meandiff
+        else:
+            adjusted_part1 = cc * part1 + dd
+
+        dataframe['adjusted'] = pd.concat([adjusted_part1, part2])
     else:
-        raise Exception("Select 'first' or 'last' for part to adjust")
+        raise Exception("select 'first' or 'last' for part to adjust")
 
-    dataframe['adjusted'] = pd.concat([adjusted_part1, adjusted_part2])
-
-    if check_adjustment:
-        tolerance = 0.05 # TODO: für monthly weniger streng als für daily
-        B, corr = adjustment_params(dataframe[['adjusted','refdata']].rename(columns={'adjusted':'testdata'}),
-                                    breaktime)
-
-        print 'B after adjustment: %s' %str(B.flatten())
-
-        if not compare_B(B, adjust_param, tolerance):
-            if nbr_rec > 20:
-                print 'B tolerance after %i retries not reached' % nbr_rec
-                raise Exception('6: B tolerance after adjustment not reached')
-            # !!!!!!!!!!RECURSIVE PART!!!!!!!!!!!!!!!
-            # Take adjusted data from first iteration
-            dataframe = dataframe[['adjusted', 'refdata']].rename(columns={'adjusted': 'testdata'})
-            # Retry adjustment with adjusted data and new B
-            adjusted, adj_setting = adjustment(dataframe, B, breaktime, adjust_param, adjust_part, return_part,
-                                               check_adjustment=True,nbr_rec=nbr_rec+1)
-
-            dataframe['adjusted'] = adjusted
-
-            if return_part == 'all':
-                return dataframe['adjusted'], adj_setting
-            elif return_part == 'last':
-                return dataframe['adjusted'][breaktime+pd.DateOffset(1):], adj_setting
-            elif return_part == 'first':
-                return dataframe['adjusted'][:breaktime], adj_setting
-            else:
-                raise Exception("Select 'first' , 'last' or 'all' for part to return")
-
-    # !!!!!!!!!!ESCAPE PART!!!!!!!!!!!!!!!
-    adj_setting = {'slope': cc, 'intercept': dd, 'part1_B1': B[0][0], 'part1_B2': B[0][1],
-                   'part2_B1': B[1][0], 'part2_B2': B[1][1]}
+    adj_setting = {'slope': cc, 'intercept': dd}
 
     if return_part == 'all':
         return dataframe['adjusted'], adj_setting
@@ -129,6 +105,9 @@ def adjustment(data_daily, B, breaktime, adjust_param = 'both', adjust_part='fir
         return dataframe['adjusted'][:breaktime], adj_setting
     else:
         raise Exception("Select 'first' , 'last' or 'all' for part to return")
+
+
+
 
 def adjustment_params(data, breaktime):
     '''
@@ -154,20 +133,26 @@ def adjustment_params(data, breaktime):
     #     elif i1.index.size > i2.index.size:
     #         i1 = i1[-1 * i2.index.size:]
 
-    B = []
+    B = {'b1':None, 'b2':None}
     rval = []
     pval = []
-    for data in [i1, i2]:
-        refdata = np.stack((np.ones(data['refdata'].values.size), data['refdata'].values))
+    for i,data in enumerate([i1, i2], start=1):
+
         testdata = data['testdata'].values
-        b = np.dot(np.linalg.inv(np.asmatrix(np.dot(refdata, np.transpose(refdata)))),
-                   np.matrix.transpose(np.asmatrix(np.dot(refdata, testdata))))
-        B.append(b)
-        r, p = stats.pearsonr(refdata[1], testdata)
+        refdata = data['refdata'].values
+
+        w = -1 * testdata # using approximate solutions 0 and 0 for k and d
+        # w = refdata - testdata # using approximate solutions 1 and 0 for k and d --> different B
+        l = -w
+        A = np.stack(( data['refdata'].values, np.ones(data['refdata'].values.size)))
+        b = np.dot(np.dot(np.linalg.inv(np.dot(A, np.transpose(A))), A), l)
+
+        B['b%i' %i] = b
+
+        r, p = stats.pearsonr(refdata, testdata)
         rval.append(r)
         pval.append(p)
 
-    B = np.squeeze(np.asarray(B)) # B= regression coefficients between Refdata and Testdat
     regtest = {'R': rval, 'p': pval}
 
     #TODO: activate tests
@@ -181,131 +166,3 @@ def adjustment_params(data, breaktime):
 
 
 
-if __name__ == '__main__':
-    import warnings
-    warnings.filterwarnings('ignore')
-
-    from interface import HomogTest
-    from otherfunctions import regress
-    import matplotlib.dates as dates
-    import matplotlib.pyplot as plt
-    import pandas as pd
-    import scipy.stats as stats
-    import numpy as np
-    from cci_timeframes import get_timeframes
-
-    test_obj = HomogTest('cci_31_combined',
-                         'merra2',
-                         ['wilkoxon', 'fligner_killeen'],
-                         0.01,
-                         False,
-                         None)
-
-    df_full = test_obj.read_gpi(433291, start=test_obj.range[0], end=test_obj.range[1])
-    for i, (timeframe, breaktime) in enumerate(zip(test_obj.timeframes, test_obj.breaktimes)):
-        print ' '
-        print ' '
-        print '================%s===============' % str(breaktime)
-        sett = False
-        j=0
-        while (not sett) and j < 5:
-            df = df_full[['testdata','refdata']][timeframe[0]:timeframe[1]]
-            df = df.dropna()
-            try:
-                corr, pval = test_obj.check_corr(df)
-                df, _, _ = test_obj.group_by_breaktime(df, breaktime, min_data_size=3,
-                                                         ignore_exception=False)  # TODO: different value?
-            except:
-                print 'TESTING FAILED'
-                break
-            # Correct bias in reference data
-            df['refdata'] = test_obj.ref_data_correction(df[['testdata', 'refdata']])
-            # Calculate difference TimeSeries
-            df['Q'] = df['testdata'] - df['refdata']
-            corr, p = test_obj.check_corr(df)
-            print '------------BEFORE ADJUSTMENT-----------'
-            print ('Correlation total: corr: %f , pval: %f' %(corr, p) )
-            # Run Tests
-            _, testresult = test_obj.run_tests(data=df)
-            print testresult
-            if testresult['fligner_killeen']['h'] == 0 and testresult['wilkoxon']['h'] == 0:
-                print 'Adjustment not necessary'
-                break
-            x1 = dates.date2num([pd.Timestamp(date).to_datetime() for date in df[:breaktime].index.values])
-            x2 = dates.date2num([pd.Timestamp(date).to_datetime() for date in df[breaktime:].index.values])
-
-            slope1, inter1, r1, p1, stderr1 = stats.linregress(x1, df['testdata'][:breaktime])
-            part1 = slope1 * x1 + inter1
-            print 'Variance part 1: %f' % np.var(part1)
-            print 'Mean part 1: %f' % np.mean(part1)
-            slope2, inter2, r2, p2, stderr2 = stats.linregress(x2, df['testdata'][breaktime:])
-            part2 = slope2 * x2 + inter2
-            print 'Variance part 2: %f' % np.var(part2)
-            print 'Mean part 2: %f' % np.mean(part2)
-            if part1.size+part2.size != df.index.size:
-                part2 = part2[1:]
-            df['testdata_ress_before'] = np.concatenate((part1, part2)) # breaktime overlaps, drop it for first set
-            df[['testdata', 'testdata_ress_before']].plot()
-
-            print('Testdata_preadjust: PART 1: slope: %f, intercept: %f' % (slope1, inter1))
-            print('Testdata_preadjust: PART 2: slope: %f, intercept: %f' % (slope2, inter2))
-            print '------------AFTER ADJUSTMENT-----------'
-            try:
-                B, regtest = adjustment_params(df[['testdata', 'refdata']],
-                                               breaktime)
-
-
-                ts_adjusted, sett = adjustment(df[['testdata', 'refdata']], B, breaktime,
-                                               adjust_param='both', adjust_part='first',
-                                               return_part='all' if i == 0 else 'first' , check_adjustment=True)
-                df_full.loc[ts_adjusted.index, 'testdata'] = ts_adjusted
-                j+=1
-
-            except Exception as e:
-                print 'Adjustment failed: %s' %e
-                continue
-
-        if not sett:
-            continue
-
-        print ('regtest: %s' % str(regtest))
-
-        testdata_before = df['testdata'].copy()
-
-
-
-        df = df_full[['testdata','refdata']][timeframe[0]:timeframe[1]]
-        df=df.dropna()
-        try:
-            corr, pval = test_obj.check_corr(df)
-            df, _, _ = test_obj.group_by_breaktime(df, breaktime, min_data_size=3,
-                                                     ignore_exception=False)  # TODO: different value?
-        except:
-            print 'TESTING FAILED'
-            pass
-        df['refdata'] = test_obj.ref_data_correction(df[['testdata', 'refdata']])
-        df['Q'] = df['testdata'] - df['refdata']
-        corr, p = test_obj.check_corr(df)
-        print ('Correlation total: corr: %f , pval: %f' %(corr, p) )
-        _, testresult = test_obj.run_tests(data=df)  # wilkoxon removed, fk not!!
-        print testresult
-
-        x1 = dates.date2num([pd.Timestamp(date).to_datetime() for date in df[:breaktime].index.values])
-        x2 = dates.date2num([pd.Timestamp(date).to_datetime() for date in df[breaktime:].index.values])
-        slope1, inter1, r1, p1, stderr1 = stats.linregress(x1, df['testdata'][:breaktime])
-        part1 = slope1 * x1 + inter1
-        print 'Variance part 1: %f' % np.var(part1)
-        print 'Mean part 1: %f' % np.mean(part1)
-        slope2, inter2, r2, p2, stderr2 = stats.linregress(x2, df['testdata'][breaktime:])
-        part2 = slope2 * x2 + inter2
-
-        if part1.size+part2.size != df.index.size:
-            part2=part2[1:]
-        print 'Variance part 2: %f' % np.var(part2)
-        print 'Mean part 2: %f' % np.mean(part2)
-        df['testdata_ress_after'] = np.concatenate((part1, part2))
-        df[['testdata', 'testdata_ress_after']].plot()
-        df['testdata_before'] =testdata_before
-        print('Testdata_aftadjust: PART 1: slope: %f, intercept: %f' % (slope1, inter1))
-        print('Testdata_aftadjust: PART 2: slope: %f, intercept: %f' % (slope2, inter2))
-        #df_roll = pd.rolling_mean(df[['testdata','testdata_before']], 5, 3)

cci_timeframes.py
@@ -16,6 +16,7 @@ from otherfunctions import cci_extract, cci_string_combine
 # Order matters!!!!
 class CCITimes(object):
     def __init__(self, product, ignore_position=True, skip_times=None):
+
         timeframes = {'CCI_22_COMBINED': np.array(
             [['2011-10-01', '2014-12-31'], ['2007-01-01', '2012-07-01'],
              ['2002-07-01', '2011-10-01'], ['1998-01-01', '2007-01-01'],
@@ -95,7 +96,11 @@ class CCITimes(object):
                   'CCI_31_ACTIVE': np.array(['1991-08-01', '2015-12-31']),
                   'CCI_32_COMBINED': np.array(['1980-01-01', '2015-12-31']),
                   'CCI_33_COMBINED': np.array(['1978-10-26', '2016-12-31']),
-                  'CCI_41_COMBINED': np.array(['1978-10-26', '2016-12-31'])}
+                  'CCI_33_ACTIVE': np.array(['1991-08-05', '2016-12-31']),
+                  'CCI_33_PASSIVE': np.array(['1978-10-26', '2016-12-31']),
+                  'CCI_41_COMBINED': np.array(['1978-10-26', '2016-12-31']),
+                  'CCI_41_ACTIVE': np.array(['1991-08-05', '2016-12-31']),
+                  'CCI_41_PASSIVE': np.array(['1978-10-26', '2016-12-31'])}
 
         if product not in timeframes.keys() or product not in breaktimes.keys() or product not in ranges.keys():
             self.product = self._init_timeframes_for_adjusted(product)
@@ -115,6 +120,9 @@ class CCITimes(object):
 
         if not self.ignore_position:
             self.grid_points = SMECV_Grid_v042().get_grid_points()
+
+        if self.skip_times and not self.ignore_position and self.gpi_dep_times():
+            raise Exception('Skipping breaktimes for gpi dependent timeframes ambiguous')
 
     @staticmethod
     def as_string(datetimes):
@@ -142,7 +150,7 @@ class CCITimes(object):
         :param skip_times: list
         :return: np.array
         '''
-        return [datestrings[i] for i in range(datestrings.shape[0]) if i not in skip_times]
+        return [datestrings[i] for i in range(len(datestrings)) if i not in skip_times]
 
     def _init_timeframes_for_adjusted(self, product):
         info = cci_extract(product)
@@ -281,7 +289,7 @@ class CCITimes(object):
 
 
 if __name__ == '__main__':
-    ds = CCITimes('CCI_33_COMBINED', ignore_position=False)
+    ds = CCITimes('CCI_33_COMBINED', ignore_position=True, skip_times=[0,1,2,3,4,5,6,8])
     for gpi in [620964, 805227]:  # First Point < 37 Lat, second > 37 Lat
         times = ds.get_times(gpi, as_datetime=True)
         print 'breaktimes:'
@@ -290,10 +298,10 @@ if __name__ == '__main__':
         print times['timeframes']
         print 'ranges:'
         print times['ranges']
-        print (ds.timeframe_for_breaktime(gpi, times['breaktimes'][1]))
-        print (ds.breaktime_for_timeframe(gpi, times['timeframes'][1]))
-        print (ds.get_adjacent(gpi, times['timeframes'][1], 2))
-        print (ds.get_adjacent(gpi, times['breaktimes'][5], -2))
+        print (ds.timeframe_for_breaktime(gpi, times['breaktimes'][0]))
+        print (ds.breaktime_for_timeframe(gpi, times['timeframes'][0]))
+        #print (ds.get_adjacent(gpi, times['timeframes'][1], 2))
+        #print (ds.get_adjacent(gpi, times['breaktimes'][5], -2))
     ds = CCITimes('CCI_33_COMBINED', ignore_position=True)
     for gpi in [620964, 805227]:  # First Point < 37 Lat, second > 37 Lat
         times = ds.get_times(gpi, as_datetime=False)
@@ -303,7 +311,7 @@ if __name__ == '__main__':
         print times['timeframes']
         print 'ranges:'
         print times['ranges']
-        print (ds.timeframe_for_breaktime(gpi, times['breaktimes'][1]))
-        print (ds.breaktime_for_timeframe(gpi, times['timeframes'][1]))
-        print (ds.get_adjacent(gpi, times['timeframes'][1], 2))
-        print (ds.get_adjacent(gpi, times['breaktimes'][5], -2))
+        print (ds.timeframe_for_breaktime(gpi, times['breaktimes'][0]))
+        print (ds.breaktime_for_timeframe(gpi, times['timeframes'][0]))
+        #print (ds.get_adjacent(gpi, times['timeframes'][1], 2))
+        #print (ds.get_adjacent(gpi, times['breaktimes'][5], -2))

interface.py
@@ -37,6 +37,7 @@ class HomogTest(object):
         self.test_prod = test_prod
         self.range = CCITimes(test_prod, ignore_position=True).get_times(None, as_datetime=False)['ranges']
         self.tests = tests
+        self.testresults = {test:None for test in self.tests}
         '''
         if self.ref_prod == 'ISMN-merge':
             self.data = QDEGdata_M(products=[test_prod])
@@ -103,7 +104,7 @@ class HomogTest(object):
         '''
 
         # number of measurements and datagroups
-        df = dataframe.rename(columns={dataframe.ix[:, 0].name: 'data'})
+        df = dataframe.rename(columns={'Q': 'data'})
         df = df.dropna()
         N = df.index.size
         K = df['group'].nunique()
@@ -114,7 +115,7 @@ class HomogTest(object):
             for i in range(K):
                 subset = df.ix[df['group'] == i]
                 groupmed = subset.data.median()
-                df.ix[df['group'] == i, 'groupmed'] = groupmed  # groupmedians
+                df.ix[df['group'] == i, 'groupmed'] = groupmed  # group medians
                 df.ix[df['group'] == i, 'groupme_diff'] = np.abs(
                     subset['data'] - groupmed)  # difference data-groupmedians
 
@@ -214,7 +215,7 @@ class HomogTest(object):
     def check_corr(self, df_time):
         # Check if any data is left for testdata and reference data
         if df_time.isnull().all().refdata or df_time.isnull().all().testdata:
-            raise Exception('1: No coinciding data for the selected timeframe')
+            raise Exception('1: No data for the selected timeframe')
 
         # Check if lengths of remaining datasets equal TODO: it always is, can be removed
         if df_time['refdata'].index.size != df_time['testdata'].index.size:
@@ -297,4 +298,5 @@ class HomogTest(object):
         if type(wilkoxon['h']) is str and type(fligner_killeen['h']) is str:
             raise Exception('6: WK test and FK test failed')
 
+        self.testresults[test for test in tests]
         return data, {'wilkoxon': wilkoxon, 'fligner_killeen': fligner_killeen}

main.py
@@ -5,11 +5,13 @@ Created on Wed Mar 22 17:05:55 2017
 @author: wpreimes
 """
 from typing import Union
+import numpy as np
+import matplotlib.pyplot as plt
 import os
 from datetime import datetime
 from multiprocessing import Process, Queue
 from pynetcf.time_series import GriddedNcIndexedRaggedTs
-from otherfunctions import resample_to_monthly, csv_read_write, join_files, split, create_workfolder
+from otherfunctions import temp_resample, csv_read_write, join_files, split, create_workfolder
 from interface import HomogTest
 from cci_timeframes import CCITimes
 from save_data import Results2D, extract_adjust_infos, extract_test_infos, save_Log, GlobalResults
@@ -17,19 +19,22 @@ from grid_functions import cells_for_continent
 from adjustment import adjustment, adjustment_params
 from otherplots import show_processed_gpis, inhomo_plot_with_stats, longest_homog_period_plots
 from smecv_grid.grid import SMECV_Grid_v042
+import warnings
 
-def process_for_cells(q, workfolder, save_obj, times_obj, cells, log_file, test_prod, ref_prod, anomaly, backward_extended_timeframes,
+
+def process_for_cells(q, workfolder, save_obj, times_obj, cells, log_file, test_prod, ref_prod, anomaly,
+                      backward_extended_timeframes,
                       process_no, perform_adjustment=True):
-
     # Function for multicore processing
 
     print('Start process %i' % process_no)
     process_csv_file = 'saved_points_%s.csv' % process_no
     ########################################## TODO: Values to change
-    adjusted_data_path = os.path.join(workfolder,test_prod+'_adjusted')
-    get_all_B = False # TODO: Try this
-    min_monthly_values = 10 # If CCI data contains less values, the monthly value is resampled as nan
-    min_data_size = 3 # Minimum number of monthly values before/after break to perform homogeneity testing # TODO: different value?
+    adjusted_data_path = os.path.join(workfolder, test_prod + '_adjusted')
+    min_data_for_temp_resampling = 0.33  # If CCI data contains less values, the monthly value is resampled as nan
+    min_data_size = 3  # Minimum number of monthly values before/after break to perform homogeneity testing # TODO: different value?
+    resample_method = 'M'
+    refdata_correction_for = 'iteration'  # iteration, full or timeframe
     ##########################################
     tests = ['wilkoxon', 'fligner_killeen']
 
@@ -54,10 +59,10 @@ def process_for_cells(q, workfolder, save_obj, times_obj, cells, log_file, test_
         grid_points = (save_obj.pre_process_grid).grid_points_for_cell(cell)[0]
 
         for iteration, gpi in enumerate(grid_points):
-            #if gpi not in [369909, 365585, 375665, 320970, 322412]: continue
+            #if gpi!=358365: continue
 
-            if iteration%10 == 0:
-                print 'processing gpi %i (%i of %i)' % (gpi, iteration, grid_points.size)
+            # if iteration%10 == 0:
+            print 'processing gpi %i (%i of %i)' % (gpi, iteration, grid_points.size)
             '''
             if test_obj.ref_prod == 'ISMN-merge':
                 valid_insitu_gpis = test_obj.ismndata.gpis_with_netsta
@@ -76,32 +81,43 @@ def process_for_cells(q, workfolder, save_obj, times_obj, cells, log_file, test_
             breaktimes = times['breaktimes']
 
             homogeneous_breaktimes = []
+
+            B_compare = {'before': {'k1': np.nan, 'd1': np.nan,
+                                    'k2': np.nan, 'd2': np.nan},
+                         'after': {'k1': np.nan, 'd1': np.nan,
+                                   'k2': np.nan, 'd2': np.nan}}
+
             for i, (timeframe, breaktime) in enumerate(zip(timeframes, breaktimes)):
                 retries = 0
                 adjresult = False
                 testresult = False
+
                 # In case that after adjustment a break is still found, re-correct the reference data to
                 # the (insufficiently) corrected test data and repeat adjustment (< max_retries times)
+                original_values = df_time[timeframe[0]:timeframe[1]]
                 while retries < max_retries and (not testresult or
                                                      not test_obj.check_testresult(testresult)[1]):
 
-                    if backward_extended_timeframes: # Cut data to timeframe
+                    if backward_extended_timeframes:  # Cut data to timeframe
                         while timeframe[1] in homogeneous_breaktimes:
-                            timeframe[1] = times_obj.get_adjacent(gpi, timeframe, -1)[1] #Use previous timeframe
+                            timeframe[1] = times_obj.get_adjacent(gpi, timeframe, -1)[1]  # Use end of previous timeframe
 
-                    data_daily = df_time[timeframe[0]:timeframe[1]] # keep this for adjustment #TODO: Allow Extended Timeframes?
+                    data_daily = df_time[timeframe[0]:timeframe[1]] # change with adjustment iterations
+                        # keep this for adjustment #TODO: Allow Extended Timeframes?
                     try:  # Homogeneity Testing
-                        data_daily = data_daily.dropna()
-                        # Correct bias in reference data
-                        data_daily['refdata'] = test_obj.ref_data_correction(data_daily[['testdata', 'refdata']])
+                        data_daily = data_daily.dropna()  # TODO: activate?
                         # resample to monthly value
-                        data = resample_to_monthly(data_daily, min_monthly_values).dropna()
+                        data = temp_resample(data_daily, resample_method, min_data_for_temp_resampling).dropna()
+                        # Correct bias in reference data
+                        if refdata_correction_for == 'iteration' or (refdata_correction_for == 'timeframe' and retries == 0):
+                            data_daily['refdata'] = test_obj.ref_data_correction(data_daily[['testdata', 'refdata']])
+                            data['refdata'] = test_obj.ref_data_correction(data[['testdata', 'refdata']])
                         # Checks if there is data left and calculates spearman correlation
                         corr, pval = test_obj.check_corr(data)
-                        data, _, _= test_obj.group_by_breaktime(data,
-                                                                breaktime,
-                                                                min_data_size=min_data_size,
-                                                                ignore_exception=False)
+                        data, _, _ = test_obj.group_by_breaktime(data,
+                                                                 breaktime,
+                                                                 min_data_size=min_data_size,
+                                                                 ignore_exception=False)
 
                         # Calculate difference TimeSeries
                         data['Q'] = data['testdata'] - data['refdata']
@@ -113,15 +129,33 @@ def process_for_cells(q, workfolder, save_obj, times_obj, cells, log_file, test_
                         break
 
                     break_found_by, check_result = test_obj.check_testresult(testresult)
-                    if check_result == True: # no break was found
+
+                    try:
+                        B, corr = adjustment_params(data=data,  # todo: or monthly??
+                                                    breaktime=breaktime)
+
+                    except Exception as e:
+                        adjresult = {'adj_status': str(e)}
+                        print '%i: Adjustment failed due to exception: %s' % (gpi, e)
+                        break
+
+                    if retries == 0:
+                        B_compare['before'] = {'k1': B['b1'][0], 'd1': B['b1'][1],
+                                               'k2': B['b2'][0], 'd2': B['b2'][1]}
+                    else:
+                        B_compare['after'] = {'k1': B['b1'][0], 'd1': B['b1'][1],
+                                              'k2': B['b2'][0], 'd2': B['b2'][1]}
+
+                    if check_result == True:  # no break was found
                         homogeneous_breaktimes.append(breaktime)
+
                         if retries == 0:
-                            #print '%s: Adjustment not necessary' % str(breaktime.date())
+                            # print '%s: Adjustment not necessary' % str(breaktime.date())
                             break  # No breaks found, escape loop
                         else:  # break was removed, escape loop
                             print '%s: Break removed after %i retries' % (str(breaktime.date()), retries)
                             break
-                    elif get_all_B or perform_adjustment:  # break was found, attempt adjustment
+                    elif perform_adjustment:  # break was found, attempt adjustment
                         '''
                         # Adjustment 
                         1) Perform adjustment on the dataframe
@@ -134,26 +168,16 @@ def process_for_cells(q, workfolder, save_obj, times_obj, cells, log_file, test_
                         5) Iterate over all gpis
                         '''
                         try:  # Time Series Adjustment
-                            B, corr = adjustment_params(data=data,   #todo: or monthly??
-                                                        breaktime=breaktime)
-
-                            #TODO: point dependent adjustment parameter?
-                            '''
-                            if break_found_by == ['wilkoxon']:
-                                adjust_param = 'add'
-                            elif break_found_by == ['fligner_killeen']:
-                                adjust_param = 'mult'
+                            # TODO: point dependent adjustment parameter?
+                            if retries == 0 and testresult['wilkoxon']['h'] == 1 and testresult['fligner_killeen']['h'] == 0:
+                                adjust_param = 'd'
                             else:
                                 adjust_param = 'both'
-                            '''
                             adjusted, adjresult = adjustment(data_daily=data_daily,
                                                              B=B,
                                                              breaktime=breaktime,
-                                                             adjust_param='both',
-                                                             adjust_part='first',
-                                                             return_part='all' if i == 0 else 'first',
-                                                             check_adjustment=True)
-
+                                                             adjust_param=adjust_param,
+                                                             adjust_part='first')
 
                             adjresult.update({'adj_status': '0: Adjustment performed'})
                             df_time.loc[adjusted.index, 'testdata'] = adjusted
@@ -168,11 +192,17 @@ def process_for_cells(q, workfolder, save_obj, times_obj, cells, log_file, test_
                         break
 
                 # Merge test results and adjustresults for gpi and add to save object
-                if testresult['test_status'][0] == '0' and not test_obj.check_testresult(testresult)[1]: #todo: ugly
+                if testresult['test_status'][0] == '0' and not test_obj.check_testresult(testresult)[1]:  # todo: ugly
                     print ('%s: Could not remove break after %i retries :(' % (str(breaktime.date()), max_retries))
+                    #df_time[timeframe[0]:timeframe[1]] = original_values
                 testresult = extract_test_infos(testresult)
                 if not adjresult:
                     adjresult = {'adj_status': '9: Not adjusted'}
+
+                for befaft, Bs in B_compare.iteritems():
+                    for name, val in Bs.iteritems():
+                        adjresult[name + '_' + befaft] = val
+
                 adjresult = extract_adjust_infos(adjresult)
                 merged_results = testresult.copy()
                 merged_results.update(adjresult)
@@ -208,23 +238,24 @@ def start(test_prod, ref_prod, path, cells='global', skip_times=None, anomaly=No
     :return:
     '''
     if skip_times and perform_adjustment:
-        raise Warning('Ignoring Breaktimes with activated adjustment!')
+        warnings.warn('Ignoring Breaktimes with activated adjustment!')
 
-    times_obj = CCITimes(test_prod,ignore_position=True, skip_times=skip_times) #TODO: activate for location conditional Timeframes
+    times_obj = CCITimes(test_prod, ignore_position=True,
+                         skip_times=skip_times)  # TODO: activate for location conditional Timeframes
 
     grid = SMECV_Grid_v042()
 
     workfolder = create_workfolder(path)
 
-    log_file = save_Log(workfolder, test_prod, ref_prod, anomaly, cells)
+    log_file = save_Log(workfolder, test_prod, ref_prod, anomaly, cells, backward_extended_timeframes)
 
     if cells == 'global':
         cells = grid.get_cells()
-    elif isinstance(cells,str):
+    elif isinstance(cells, str):
         cells = cells_for_continent(cells)
 
     save_obj = Results2D(grid, workfolder, times_obj.get_times(None, as_datetime=False)['breaktimes'],
-                         cell_files_path=os.path.join(workfolder,'temp_homogtest_cellfiles' ))
+                         cell_files_path=os.path.join(workfolder, 'temp_homogtest_cellfiles'))
 
     processes = []
     q = Queue()
@@ -232,7 +263,8 @@ def start(test_prod, ref_prod, path, cells='global', skip_times=None, anomaly=No
     # Split cells in equally sized packs for multiprocessing
     for process_no, cell_pack in enumerate(list(split(cells, parallel_processes))):
         p = Process(target=process_for_cells, args=(q, workfolder, save_obj, times_obj, cell_pack,
-                                                    log_file, test_prod, ref_prod, anomaly, backward_extended_timeframes,
+                                                    log_file, test_prod, ref_prod, anomaly,
+                                                    backward_extended_timeframes,
                                                     process_no, perform_adjustment))
         processes.append(p)
         p.start()
@@ -250,7 +282,8 @@ def start(test_prod, ref_prod, path, cells='global', skip_times=None, anomaly=No
 
     # Global files and images from testing
     save_obj_glob = GlobalResults(save_obj, post_process_grid)
-    global_file_name = save_obj_glob.save_global_file(keep_cell_files=True)  # Merge test result cell files to global file
+    global_file_name = save_obj_glob.save_global_file(
+        keep_cell_files=True)  # Merge test result cell files to global file
     log_file.add_line('Merged files to global file: %s' % global_file_name)
     image_files_names = save_obj_glob.create_image_files()  # Create 2D image files from test results
     log_file.add_line('Create global image files:')
@@ -282,9 +315,9 @@ if __name__ == '__main__':
     start('CCI_41_COMBINED',
           'merra2',
           r'D:\users\wpreimes\datasets\HomogeneityTesting_data\output',
-          cells=[777,742],
+          cells='Australia',
           skip_times=None,
           anomaly=False,
-          backward_extended_timeframes = True,
+          backward_extended_timeframes=False,
           perform_adjustment=True,
-          parallel_processes=2)
+          parallel_processes=8)

otherfunctions.py
@@ -16,7 +16,6 @@ from datetime import datetime
 import csv
 
 
-
 def cci_extract(_string):
     cont = _string.upper().split('_')
     if len(cont) == 3:
@@ -102,8 +101,7 @@ def regress(data):
     dataframe = data.copy()
     out = dataframe.refdata
     dataframe = dataframe.dropna()
-    # Berechnet Korrelation zwischen Testdaten und Referenzdaten
-    R, pval = stats.pearsonr(dataframe.refdata, dataframe.testdata)
+    R, pval = stats.pearsonr(dataframe.refdata, dataframe.testdata) # Correlation between Refdata and Testdata
 
 
     if R < 0 or np.isnan(R):
@@ -122,19 +120,23 @@ def regress(data):
     return pd.Series(index=dataframe.index, data=np.squeeze(np.asarray(out))), R, pval, ress
 
 
-def resample_to_monthly(df, threshold=10):
+def temp_resample(df, how='M', threshold=0.33):
     '''
     Resample a dataframe to monthly values, if the number of valid values (not nans) in a month
     is smaller than the defined threshold, the monthly resample will be NaN
 
-    :param threshold: int
-        minimum number of days to resample to monthly
+    :param how: str
+        Time frame for temporal resampling, M = monthly, 10D = 10daily,...
+    :param threshold: float
+        % of valid days (not nan) in timeframe defined in 'how'
     :return: pd.DataFrame
         The monthly resampled Data
     '''
     concat = []
     for column in df.columns.values:
-        resampled = df[[column]].groupby(pd.TimeGrouper("M")).filter(lambda g: g.count() > threshold).resample('M').mean()
+        resampled = df[[column]].groupby(pd.TimeGrouper(how))\
+                                .filter(lambda g: g.count() > round(g.count()*threshold))\
+                                .resample(how).mean()
         concat.append(resampled)
 
     return pd.concat(concat, axis = 1)

otherplots.py
@@ -21,11 +21,10 @@ from save_data import load_Log
 from smecv_grid.grid import SMECV_Grid_v042
 import xarray as xr
 from interface import HomogTest
-from otherfunctions import resample_to_monthly
+from otherfunctions import temp_resample
 
-import platform
 
-def valid_months_plot(workdir, testproduct, refproduct, min_monthly_values):
+def valid_months_plot(workdir, testproduct, refproduct, resample_method, min_monthly_values):
 
     #NOT IN PROCESS
 
@@ -35,12 +34,12 @@ def valid_months_plot(workdir, testproduct, refproduct, min_monthly_values):
     #Count values BEFORE breaktime
 
     #Count value AFTER breaktime
-    time_obj = CCITimes(testproduct)
+    time_obj = CCITimes(testproduct,ignore_position=True)
 
     grid = SMECV_Grid_v042() # type: CellGrid()
 
 
-    #grid = grid.subgrid_from_cells([564]) #TODO: Delete
+    #grid = grid.subgrid_from_cells([777]) #TODO: Delete
 
     gpis = grid.get_grid_points()[0]
     lons = grid.get_grid_points()[1]
@@ -49,12 +48,12 @@ def valid_months_plot(workdir, testproduct, refproduct, min_monthly_values):
 
     test_obj = HomogTest(testproduct, refproduct,['wilkoxon', 'fligner_killeen'], 0.01, False,None)
     DF_Points = pd.DataFrame(index = gpis, data = {'lon': lons, 'lat': lats} )
-    for breaktime in time_obj.get_times(ignore_conditions=True)['breaktimes']:
+    for breaktime in time_obj.get_times()['breaktimes']:
         DF_Points['before %s' % breaktime] = np.nan
         DF_Points['after %s' % breaktime] = np.nan
 
     for iteration, gpi in enumerate(gpis):
-        times = time_obj.get_times(ignore_conditions=True, as_datetime=False)
+        times = time_obj.get_times(as_datetime=False)
         starts = np.flipud(np.array([timeframe[0] for timeframe in times['timeframes']]))
         ends = np.flipud(np.array([timeframe[1] for timeframe in times['timeframes']]))
         breaktimes = np.flipud(times['breaktimes'])
@@ -70,7 +69,7 @@ def valid_months_plot(workdir, testproduct, refproduct, min_monthly_values):
                 end = datetime.strptime(end, '%Y-%m-%d')
 
                 df_subtime = df_time[['testdata']][start:end].dropna()
-                df_resample = resample_to_monthly(df_subtime, min_monthly_values)
+                df_resample = temp_resample(df_subtime, resample_method, min_monthly_values)
                 df_group, len_bef, len_aft = \
                     test_obj.group_by_breaktime(df_resample, breaktime, 3, ignore_exception=True)
 
@@ -489,4 +488,4 @@ if __name__ == '__main__':
     #inhomo_plot_with_stats(r'H:\HomogeneityTesting_data\output\v5',"HomogeneityTestResult_2007-01-01_image.nc")
     # compare_RTM_RTG(r'H:\HomogeneityTesting_data\output\CCI31EGU','merra2')
     # show_tested_gpis(r"H:\HomogeneityTesting_data\output\v15","HomogeneityTest_merra2_2011-10-01")
-    valid_months_plot(r'H:\HomogeneityTesting_data\output\CCI_available_data_plots', 'CCI_33_COMBINED','merra2', 10)
+    valid_months_plot(r'H:\HomogeneityTesting_data\output\CCI_available_data_plots', 'CCI_33_COMBINED','merra2', 'M', 0.33)

save_data.py
@@ -17,7 +17,7 @@ from smecv_grid.grid import SMECV_Grid_v042
 
 
 class save_Log(object):
-    def __init__(self, workfolder, test_prod, ref_prod, anomaly, cells):
+    def __init__(self, workfolder, test_prod, ref_prod, anomaly, cells, backward_extended_timeframes):
         # type: (str, str, str, str, list) -> None
         self.workfolder = workfolder
 
@@ -28,6 +28,7 @@ class save_Log(object):
             f.write('Reference Product: %s \n' % ref_prod)
             f.write('Anomaly data: %s \n' % str(anomaly))
             f.write('Input Cells: %s \n' % cells)
+            f.write('Backward Extended Timeframes: %s \n' % str(backward_extended_timeframes))
             f.write('=====================================\n')
             f.write('\n')
 
@@ -37,18 +38,20 @@ class save_Log(object):
         with open(os.path.join(self.workfolder, 'log.txt'), 'a') as f:
             f.write(string + '\n')
 
+
 class load_Log(object):
     def __init__(self, workfolder):
         self.workfolder = workfolder
 
     def get_products(self):
-        with open(os.path.join(self.workfolder,'log.txt'), mode='r') as f:
+        with open(os.path.join(self.workfolder, 'log.txt'), mode='r') as f:
             lines = [line.rstrip('\n') for line in f]
 
         test_prod = lines[2].split(' ')[2]
         ref_prod = lines[3].split(' ')[2]
 
         return {'test_prod': test_prod, 'ref_prod': ref_prod}
+
 
 class Results2D(object):
     def __init__(self, grid, path, breaktimes, cell_files_path=None):
@@ -84,7 +87,6 @@ class Results2D(object):
     def reset_data(self):
         self.data = {breaktime: {'gpi': []} for breaktime in self.breaktimes}
 
-
     def add_data(self, gpi, breaktime, data_dict):
         '''
         Fill buffer by adding data for gpi
@@ -109,10 +111,10 @@ class Results2D(object):
         for breaktime, data_dict in self.data.iteritems():
             df = pd.DataFrame.from_dict(data_dict)
             df = df.set_index('gpi')
-            df = df.rename(columns={column_name : column_name + '_' + breaktime for column_name in df.columns.values})
+            df = df.rename(columns={column_name: column_name + '_' + breaktime for column_name in df.columns.values})
             dataframes.append(df)
 
-        df = pd.concat(dataframes, axis=1) #type: pd.DataFrame
+        df = pd.concat(dataframes, axis=1)  # type: pd.DataFrame
 
         if not df.index.is_unique:
             raise Exception('df gpis not unique')
@@ -128,10 +130,10 @@ class Results2D(object):
                 if loc_id not in self.global_gpis:
                     self.global_gpis.append(loc_id)
                 else:
-                    raise Exception('Trying to set existing GPI %i' %loc_id)
+                    raise Exception('Trying to set existing GPI %i' % loc_id)
         return_gpis = self.global_gpis
         self.reset_data()
-        self.global_gpis=[]
+        self.global_gpis = []
         return return_gpis
 
 
@@ -147,11 +149,11 @@ def extract_test_infos(data_dict):
         if type(wk) is str:
             # wk test failed and contains error message
             wk = np.nan
-            status = 6 # This is just an identification number, no math meaning
+            status = 6  # This is just an identification number, no math meaning
         if type(fk) is str:
             # fk failed and contains error message
             fk = np.nan
-            status = 7 # This is just an identification number, no math meaning
+            status = 7  # This is just an identification number, no math meaning
 
         all = 4.0
         if wk == 1:
@@ -169,17 +171,19 @@ def extract_adjust_infos(data_dict):
     # type: (dict) -> dict
     status = int(data_dict['adj_status'][0])
     if status not in [0]:
-        return {'slope': np.nan, 'intercept': np.nan, 'part1_B1': np.nan, 'part1_B2': np.nan,
-                'part2_B1': np.nan, 'part2_B2': np.nan, 'adj_status': status}
+        return {'slope': np.nan, 'intercept': np.nan,
+                'k1_before': np.nan, 'k2_before': np.nan,
+                'd1_before': np.nan, 'd2_before': np.nan,
+                'k1_after': np.nan, 'k2_after': np.nan,
+                'd1_after': np.nan, 'd2_after': np.nan,
+                'adj_status': status}
     else:
         data_dict['adj_status'] = status
 
-        return data_dict  #DataDict from Adjustment incl. status
-
+        return data_dict  # DataDict from Adjustment incl. status
 
 
 class GlobalResults(Results2D):
-
     def __init__(self, baseObject, post_process_grid):
         self.__class__ = type(baseObject.__class__.__name__,
                               (self.__class__, baseObject.__class__),
@@ -193,12 +197,10 @@ class GlobalResults(Results2D):
 
         self.post_process_grid = post_process_grid
 
-
     def save_global_file(self, keep_cell_files=False):
 
         with GriddedPointData(self.cell_files_path, grid=self.post_process_grid,
                               fn_format='{:04d}.nc') as nc:
-
             nc.to_point_data(os.path.join(self.path, self.fn_global))
 
         if not keep_cell_files:
@@ -211,42 +213,42 @@ class GlobalResults(Results2D):
             'test_results': 'Break detection classes by Hopothesis tests.'
                             '1 = WK only, 2 = FK only, 3 = WK and FK, 4 = None',
             'test_status': '0 = Testing performed,'
-                      '1 = No coinciding data for timeframe,'
-                      '2 = Test TS and Ref TS do not match,'
-                      '3 = Spearman correlation too low,'
-                      '4 = Min. Dataseries len. not reached,'
-                      '5 = neg/nan correl. aft. bias corr.,'
-                      '6 = Error during WK testing,'
-                      '7 = Error during FK testing,'
-                      '8 = WK test and FK test failed,'
-                      '9 = Error reading gpi',
+                           '1 = No data for timeframe,'
+                           '2 = Test TS and Ref TS do not match,'
+                           '3 = Spearman correlation too low,'
+                           '4 = Min. Dataseries len. not reached,'
+                           '5 = neg/nan correl. aft. bias corr.,'
+                           '6 = Error during WK testing,'
+                           '7 = Error during FK testing,'
+                           '8 = WK test and FK test failed,'
+                           '9 = Error reading gpi',
             'adj_status': '0: Adjustment performed,'
-                     '1 = No adjustment necessary,'
-                     '2 = negative correlation,'
-                     '3 = positive correlation insignificant,'
-                     '4 = Model param 1 not sufficiently adapted,'
-                     '5 = Model param 2 not sufficiently adapted,'
-                     '6 = B tolerance after adj. not reached,'
-                     '9 = Not adjusted' }
+                          '1 = No adjustment necessary,'
+                          '2 = negative correlation,'
+                          '3 = positive correlation insignificant,'
+                          '4 = Model param 1 not sufficiently adapted,'
+                          '5 = Model param 2 not sufficiently adapted,'
+                          '6 = B tolerance after adj. not reached,'
+                          '9 = Not adjusted'}
 
         global_file = xr.open_dataset(os.path.join(self.path, self.fn_global))
         df = global_file.to_dataframe()
 
-        filenames =[]
+        filenames = []
         for breaktime_str in self.breaktimes:
-            df_breaktime = df[['lat','lon']]
+            df_breaktime = df[['lat', 'lon']]
             for col_name in df.columns.values:
                 if breaktime_str in col_name:
                     [var, breaktime] = re.split(r'[_](?=[0-9])', col_name)
                     df_breaktime[var] = df[col_name]
             df_breaktime['location_id'] = df['location_id'].astype(int)
-            df_breaktime = df_breaktime.sort_values(['lat','lon']) \
-                                       .set_index(['lat','lon'])
+            df_breaktime = df_breaktime.sort_values(['lat', 'lon']) \
+                .set_index(['lat', 'lon'])
 
             global_image = df_breaktime.to_xarray()
 
             for name, val in file_meta_dict.iteritems():
-                global_image.variables[name].attrs['Values']=val
+                global_image.variables[name].attrs['Values'] = val
 
             filename = self.fn_images_base % breaktime_str
             global_image.to_netcdf(os.path.join(self.path, filename))
@@ -256,11 +258,11 @@ class GlobalResults(Results2D):
 
 
 if __name__ == '__main__':
-    gpis = range(392888,392901)+range(391448,391461)
+    gpis = range(392888, 392901) + range(391448, 391461)
     grid = SMECV_Grid_v042()
     save_obj = Results2D(grid, r'C:\Temp', ['2000-01-01'])
     for gpi in gpis:
-        save_obj.add_data(gpi, '2000-01-01', {'testresult':{'status_test':'1'},
+        save_obj.add_data(gpi, '2000-01-01', {'testresult': {'status_test': '1'},
                                               'adjresult': {'status_adj': '1'}})
     global_gpis = save_obj.save_to_gridded_netcdf()
 